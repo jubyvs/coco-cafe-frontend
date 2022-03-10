a9a23b808a91cba12330f2614121d04f91f218f5
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2022-03-10 00:37:36 -0500

    Finished Home structure

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,20 +3,19 @@ import React from 'react';
 
 import NavigationTop from '../Header/NavigationTop';
 import Footer from '../Footer/footer';
-import ResponsiveCarousel from '../Home/carousal';
+import ResponsiveCarousel from './carousal';
+import Menu from './menu';
 
 const Home = () => {
     return(
-<React.Fragment>
-    <NavigationTop />
-    <Bootstrap.Container className="pt-5 mt-5 text-center text-dark main-container" fluid>
-        <ResponsiveCarousel />
-        <Bootstrap.Row>
-            
-        </Bootstrap.Row>
-    </Bootstrap.Container>
-    <Footer />
-</React.Fragment>
+        <React.Fragment>
+            <NavigationTop />
+            <Bootstrap.Container className="pt-5 mt-5 text-center text-dark main-container" fluid>
+                <ResponsiveCarousel />
+                <Menu />
+            </Bootstrap.Container>
+            <Footer />
+        </React.Fragment>
     );
 }
 
---
+++ src/Home/menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as Bootstrap from 'react-bootstrap';
+
+const Menu = () => {
+    return(
+        <Bootstrap.Row xs={1} md={4} className="py-5 g-5">
+            {Array.from({ length: 6 }).map((_, idx) => (
+                <Bootstrap.Col>
+                <Bootstrap.Card>
+                    <Bootstrap.Card.Img variant="top" src="holder.js/100px160" />
+                    <Bootstrap.Card.Body>
+                    <Bootstrap.Card.Title>Card title</Bootstrap.Card.Title>
+                    <Bootstrap.Card.Text>
+                        This is a longer card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit longer.
+                    </Bootstrap.Card.Text>
+                    </Bootstrap.Card.Body>
+                </Bootstrap.Card>
+                </Bootstrap.Col>
+            ))}
+        </Bootstrap.Row>
+    );
+}
+
+export default Menu;
